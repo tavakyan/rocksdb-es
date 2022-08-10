762e9527cd446c8ecc7ff06bdb83a0e6cdad7dec
tavakyan <tavakyan@protonmail.com>
2022-08-10 10:56:24 -0700

    added view repository

--- src/repository/event.rs
+++ src/repository/event.rs
@@ -1,28 +1,47 @@
 use async_trait::async_trait;
+use cqrs_es::persist::{
+    PersistedEventRepository, PersistenceError, ReplayStream, SerializedEvent, SerializedSnapshot,
+};
 use cqrs_es::Aggregate;
-use cqrs_es::persist::{PersistedEventRepository, PersistenceError, ReplayStream, SerializedEvent, SerializedSnapshot};
 
 pub struct RocksDbEventRepository;
 
 #[async_trait]
 impl PersistedEventRepository for RocksDbEventRepository {
-    async fn get_events<A: Aggregate>(&self, aggregate_id: &str) -> Result<Vec<SerializedEvent>, PersistenceError> {
+    async fn get_events<A: Aggregate>(
+        &self,
+        aggregate_id: &str,
+    ) -> Result<Vec<SerializedEvent>, PersistenceError> {
         todo!()
     }
 
-    async fn get_last_events<A: Aggregate>(&self, aggregate_id: &str, last_sequence: usize) -> Result<Vec<SerializedEvent>, PersistenceError> {
+    async fn get_last_events<A: Aggregate>(
+        &self,
+        aggregate_id: &str,
+        last_sequence: usize,
+    ) -> Result<Vec<SerializedEvent>, PersistenceError> {
         todo!()
     }
 
-    async fn get_snapshot<A: Aggregate>(&self, aggregate_id: &str) -> Result<Option<SerializedSnapshot>, PersistenceError> {
+    async fn get_snapshot<A: Aggregate>(
+        &self,
+        aggregate_id: &str,
+    ) -> Result<Option<SerializedSnapshot>, PersistenceError> {
         todo!()
     }
 
-    async fn persist<A: Aggregate>(&self, events: &[SerializedEvent], snapshot_update: Option<(String, serde_json::value::Value, usize)>) -> Result<(), PersistenceError> {
+    async fn persist<A: Aggregate>(
+        &self,
+        events: &[SerializedEvent],
+        snapshot_update: Option<(String, serde_json::value::Value, usize)>,
+    ) -> Result<(), PersistenceError> {
         todo!()
     }
 
-    async fn stream_events<A: Aggregate>(&self, aggregate_id: &str) -> Result<ReplayStream, PersistenceError> {
+    async fn stream_events<A: Aggregate>(
+        &self,
+        aggregate_id: &str,
+    ) -> Result<ReplayStream, PersistenceError> {
         todo!()
     }
 
--- src/repository/view.rs
+++ src/repository/view.rs
@@ -1,1 +1,32 @@
+use std::marker::PhantomData;
 
+use async_trait::async_trait;
+use cqrs_es::persist::{PersistenceError, ViewContext, ViewRepository};
+use cqrs_es::{Aggregate, View};
+
+/// A RockDB backed query repository for use in backing a `GenericQuery`.
+pub struct RocksDbViewRepository<V, A> {
+    _phantom: PhantomData<(V, A)>,
+}
+
+#[async_trait]
+impl<V, A> ViewRepository<V, A> for RocksDbViewRepository<V, A>
+where
+    V: View<A>,
+    A: Aggregate,
+{
+    async fn load(&self, view_id: &str) -> Result<Option<V>, PersistenceError> {
+        todo!()
+    }
+
+    async fn load_with_context(
+        &self,
+        view_id: &str,
+    ) -> Result<Option<(V, ViewContext)>, PersistenceError> {
+        todo!()
+    }
+
+    async fn update_view(&self, view: V, context: ViewContext) -> Result<(), PersistenceError> {
+        todo!()
+    }
+}
