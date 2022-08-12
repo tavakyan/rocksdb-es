821b0e7a0ba03efb3e802f79614a51420b0104f0
tavakyan <tavakyan@protonmail.com>
2022-08-11 17:35:05 -0700

    added error

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,17 @@
+use thiserror::Error;
 
+/// RocksDbAggregateError
+#[derive(Error, Debug)]
+pub enum RocksDbAggregateError {
+    #[error("Error with optimistic locking")]
+    OptimisticLock,
+    #[error("Error with deserializing aggregate")]
+    DeserializationError,
+    #[error("Error opening path file for aggregate persistence")]
+    PathError,
+    #[error("Unknown error in aggregate.")]
+    UnknownError,
+}
+
+/// Custom result type that returns a type and a RocksDbAggregateError.
+pub type Result<T> = std::result::Result<T, RocksDbAggregateError>;
