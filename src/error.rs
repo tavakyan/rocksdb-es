aaf50afdb242d20e72d960d62b5a28fa5e7fb8be
tavakyan <tavakyan@protonmail.com>
2022-08-11 17:38:31 -0700

    adding persist conversion trait

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use cqrs_es::persist::PersistenceError;
 use thiserror::Error;
 
 /// RocksDbAggregateError
@@ -15,3 +16,29 @@ pub enum RocksDbAggregateError {
 
 /// Custom result type that returns a type and a RocksDbAggregateError.
 pub type Result<T> = std::result::Result<T, RocksDbAggregateError>;
+
+impl From<serde_json::Error> for RocksDbAggregateError {
+    fn from(err: serde_json::Error) -> Self {
+        match err.classify() {
+            serde_json::error::Category::Data | serde_json::error::Category::Syntax => {
+                RocksDbAggregateError::DeserializationError
+            }
+            serde_json::error::Category::Io | serde_json::error::Category::Eof => {
+                RocksDbAggregateError::UnknownError
+            }
+        }
+    }
+}
+
+impl From<RocksDbAggregateError> for PersistenceError {
+    fn from(err: RocksDbAggregateError) -> Self {
+        match err {
+            RocksDbAggregateError::OptimisticLock => PersistenceError::OptimisticLockError,
+            RocksDbAggregateError::PathError => PersistenceError::ConnectionError(Box::new(err)),
+            RocksDbAggregateError::DeserializationError => {
+                PersistenceError::UnknownError(Box::new(err))
+            }
+            RocksDbAggregateError::UnknownError => PersistenceError::UnknownError(Box::new(err)),
+        }
+    }
+}
